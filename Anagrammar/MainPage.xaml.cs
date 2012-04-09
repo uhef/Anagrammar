1d364b75120d90115117a3d41f04a50bbf2d8f24
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2012-04-09 12:37:13 +0300

    Create UI layout

--- Anagrammar/MainPage.xaml.cs
+++ Anagrammar/MainPage.xaml.cs
@@ -12,13 +12,15 @@ namespace Anagrammar
         public MainPage()
         {
             InitializeComponent();
+
+            sourceWord.GotFocus += (sender, args) => { sourceWord.Text = ""; };
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             var xml = XDocument.Load("kotus-sanalista_v1.xml");
 
-            wordListStatus.Text = String.Format("Word Count: {0}", WordCount(xml));
+            anagrammarResults.Text += String.Format(" Word Count: {0}", WordCount(xml));
 
             base.OnNavigatedTo(e);
         }
